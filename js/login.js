483452ab417a37adfebae46c65a162723da4f20e
Christian <christig@ucm.es>
2018-05-06 18:26:30 +0200

    Test fetch

--- js/login.js
+++ js/login.js
@@ -1,17 +1,24 @@
 
 $(document).ready(function() {
-    var url= "http://christian-ubuntu:5050/usuario";
+    var url= "https://christian-ubuntu:5050/usuario";
+
+    //$("#login-btn").click(function(){
+ 	//	var xmlhttp = new XMLHttpRequest();
+    //    xmlhttp.open("GET", url, true);
+    //    xmlhttp.onload = function(e) {
+    //        var data = JSON.parse(this.response);
+    //        alert(data);
+    //    }
+    //    xmlhttp.send();	
+    //});
 
     $("#login-btn").click(function(){
- 		var xmlhttp = new XMLHttpRequest();
-        xmlhttp.open("GET", url, true);
-        xmlhttp.onload = function(e) {
-            var data = JSON.parse(this.response);
-            alert(data);
-        }
-        xmlhttp.send();	
+        fetch(url)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(myJson) {
+            console.log(myJson);
+        });
     });
  });
- 
- 
- 
